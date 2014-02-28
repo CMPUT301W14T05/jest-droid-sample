d3416ddcd7fd37c06f6816cb662f16c5afbffccb
slmyers <slmyers@ualberta.ca>
2014-02-27 23:01:35 -0700

    added throwaway prototype method to post document to index created by app.

--- src/com/searchly/Jesd/JesdMainActivity.java
+++ src/com/searchly/Jesd/JesdMainActivity.java
@@ -12,17 +12,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
 import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestResult;
+import io.searchbox.core.Index;
 import io.searchbox.indices.CreateIndex;
 
 public class JesdMainActivity extends Activity {
 
     final Context context = this;
     private JestClient jestClient;
-
+    final String INDEX_NAME = "articles";
+    private String indexName;
     /**
      * Called when the activity is first created.
      */
@@ -33,8 +37,11 @@ public class JesdMainActivity extends Activity {
 
         requestApiKey();
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new ConnectButtonListener());
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new IndexButtonListener());
+        
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new IndexArticleButtonListener());
     }
 
     @SuppressWarnings("deprecation")
@@ -61,8 +68,18 @@ public class JesdMainActivity extends Activity {
     	jestClient = jestClientFactory.getObject();
 
     }
+    
+    public class IndexArticleButtonListener implements View.OnClickListener {
+    	final String ARTICLE_NAME = "lotr";
+		@Override
+		public void onClick(View arg0) {
+			new CreateArticleTask().execute(ARTICLE_NAME);
+			
+		}
+    	
+    }
 
-    public class ConnectButtonListener implements View.OnClickListener {
+    public class IndexButtonListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
             // get prompts.xml view
@@ -82,7 +99,8 @@ public class JesdMainActivity extends Activity {
                     .setPositiveButton("OK",
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int id) {
-                                    new CreateIndexTask().execute(userInput.getText().toString());
+                                	indexName = userInput.getText().toString();
+                                    new CreateIndexTask().execute(indexName);
                                 }
                             })
                     .setNegativeButton("Cancel",
@@ -127,5 +145,71 @@ public class JesdMainActivity extends Activity {
             }
         }
     }
+    
+    class CreateArticleTask extends AsyncTask<String, Void, JestResult> {
+    	
+    	private Exception exception;
+        private JestResult result;
+		
+		protected JestResult doInBackground(String... articleName) {
+			try {
+				DummyObject dummy = new DummyObject();
+				dummy.setField1("cats");
+				dummy.setField2("kittens");
+				
+				Gson gson = new Gson();
+				
+				String source = gson.toJson(dummy);
+				
+				Index index = new Index.Builder(source).index(indexName).type("dummy").build();
+				result = jestClient.execute(index);
+				return result;
+			} catch (Exception e){
+				this.exception = e;
+				return null;
+			}
+		}
+    	
+		protected void onPostExecute(JestResult feed){
+			if (exception == null) {
+                if (result.isSucceeded()) {
+                    simpleAlert("indexed article", result.getJsonString());
+                } else {
+                    simpleAlert("Could not index article", result.getJsonString());
+                }
+            } else {
+                simpleAlert("Exception occurred", exception.getMessage());
+            }
+		}
+    }
+    
+    class DummyObject{
+    	private String field1;
+    	private String field2;
+    	
+    	
+    	public String getField1() {
+			return field1;
+		}
 
+
+		public void setField1(String field1) {
+			this.field1 = field1;
+		}
+
+
+		public String getField2() {
+			return field2;
+		}
+
+
+		public void setField2(String field2) {
+			this.field2 = field2;
+		}
+
+
+		public DummyObject(){
+    		super();
+    	}
+    }
 }
